612c55cf86361b93bc2ab5440fad8a559aaf0764
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-04-05 10:23:20 +0100

    added a write to basket method

---
+++ ProjectW14/ADOClassLibrary/DBAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOClassLibrary
+{
+    public class DBAccess
+    {
+        Entities Context;//Injection.
+        public DBAccess(Entities GivenContext)//Construction.
+        {
+            Context = GivenContext;
+        }
+
+        //Maybe make this virtual later to mock test.
+        public void addItemtoDB(Basket anItem)//:=Write.
+        {
+            using (Context)
+            {
+                Context.Baskets.Add(anItem);
+                Context.SaveChanges();
+            }
+                
+        }
+    }
+}
